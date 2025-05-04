7379c44772ac531096c05f5a3cff386df5d5041a
adyabajpai21 <adya.bajpai@chargezone.co.in>
2025-05-04 16:43:08 +0530

    vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 const repoName = 'aspire-website'; // Replace with your actual repo name if different
+const isVercel = process.env.VERCEL === '1';
 
 const nextConfig = {
   reactStrictMode: true,
@@ -9,7 +10,8 @@ const nextConfig = {
     optimizeCss: true,
   },
   output: 'export',
-  assetPrefix: isProd ? `/${repoName}/` : '',
+  assetPrefix: isVercel ? '' : (isProd ? `/${repoName}/` : ''),
+  basePath: isVercel ? '' : (isProd ? `/${repoName}` : ''),
   images: {
     domains: ['localhost', 'images.unsplash.com'],
     unoptimized: true,
